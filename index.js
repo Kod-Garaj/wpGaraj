1892de179f4e932249c56348b2b9c170118954eb
Hakan Saltan <32411666+HakanSaltan@users.noreply.github.com>
2022-05-05 22:25:55 +0300

    Windows Etkileşimi Sağlandı

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const { execAsync } = require("./utils");
+// const { execAsync } = require("./utils");
 const { execSync } = require("child_process");
 const fs = require('fs');
 const port = 3000;
+var cors = require('cors')
 
+var corsOptions = {
+  origin: 'http://localhost:8000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 app.use(express.json()); // for parsing application/json
 
 app.get("/", (req, res) => {
@@ -14,18 +21,25 @@ app.get("/", (req, res) => {
 
 app.post("/calistir", async (req, res) => {
   const { url } = req.body;
+  const isWin = process.platform === "win32";
 
   const repoUrl = "https://github.com/Kod-Garaj/react-native-ref.git";
-  const basePath = `${__dirname}/siteler/${url}/`;
+  let basePath = `${__dirname}/siteler/${url}/`;
+  basePath = isWin ? basePath.replaceAll("/", "\\") : basePath;
   const komutlar = [
     `git clone ${repoUrl} ${basePath}`,
     `npm install --prefix ${basePath}`,
-    `cd ${basePath} && react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res && cd android && ./gradlew assembleDebug`,
+    `cd ${basePath} && react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res && cd android && ${ isWin ? "gradlew" : "./gradlew" } assembleDebug`,
     // `cd ${basePath} && react-native run-android --variant=release`,
   ];
 
   if (fs.existsSync(`${basePath}`)) {
-    execSync(`rm -rf ${basePath}`);
+    if (isWin) {
+      execSync(`rmdir /Q /S ${basePath}`);
+    }
+    else {
+      execSync(`rm -rf ${basePath}`);
+    }
     // return res.json({
     //   status: false,
     //   message: "Bu sitenin önceden çalıştırılması var.",
